cb64ca41435ca4e40f0dace075b1e11f43c6d4c4
Heaveria <me@heaveria.com>
2021-05-02 00:15:24 +0200

    On load, checks if user is logged in.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+/* On website load, checks session if user is logged in.
+ * If logged in, changes user interface is adapted.
+ * If failed, nothing happens and event is logged.
+ */
 $(document).ready(function () {
-    if(localStorage.userNation.length > 1)
+    try
     {
-        $('#loginBtn').text(localStorage.userNation);
+        if(localStorage.userNation.length > 1)
+        {
+            console.log(`${localStorage.userNation} is logged in.`)
+            $('#loginBtn').text(localStorage.userNation);
+        }
+    }
+    catch(err)
+    {
+        console.log(`No user is logged in. `)
     }
 })
