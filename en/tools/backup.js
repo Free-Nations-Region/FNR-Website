8c597f04c3c58bb3209614611a49a1ee08fba110
Heaveria <me@heaveria.com>
2021-05-08 19:03:14 +0200

    Works and gives visual feedback. Not complete.

--- en/tools/backup.js
+++ en/tools/backup.js
@@ -17,3 +17,92 @@ $(document).ready(function ()
         window.location.replace(`/FNR-Website/en/login.html?redirect=${window.location.href}`);
     }
 })
+
+// Dispatch Backup Tool
+async function backup()
+{
+    // 1. Disable Form & Set jQuery Value for caching efficiency.
+    let submitBtn = $('#submitButton');
+    let progressAlert = $('.progressAlert');
+    let progressBar = $('.progressBar');
+    let progressBarPercent = $('#progressBar');
+    submitBtn.prop("disabled",true);
+    submitBtn.text(`Please wait...`);
+
+    // 2. Get/Set Values
+    let uAgent = localStorage.userNation;
+    let nName = $('#nationName').val();
+    let throttle = $('#throttle').val();
+    let url = `https://www.nationstates.net/cgi-bin/api.cgi`;
+    let uAgentParam = `userAgent=Script%20by%20Heaveria.%20In%20use%20by%20${uAgent}`;
+
+    // 3. Visual Feedback (Progress)
+    progressAlert.show();
+
+    // 4. Try statement to catch errors and stop execution.
+    try
+    {
+        // 5. Get a list of all dispatches.
+        let dispatchList = []
+        await fetch(`${url}?${uAgentParam}&nation=${nName}&q=dispatchlist`)
+            .then(res => {
+                if (res.status === 404) {
+                    throw Error(`Nation does not exist. (404)`)
+                }
+                return res.text()
+            })
+            .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+            .then(data => {
+                let rawList = data.getElementsByTagName("DISPATCH");
+                for (let i = 0; i < rawList.length; i++)
+                {
+                   dispatchList.push(rawList[i].attributes[0].value);
+                }
+            });
+        await sleep(throttle); // Rate limit
+
+        // 5. Update visual feedback
+        progressBar.show();
+
+        // 6. Get text for each dispatch.
+        let textList = [];
+        for (let i in dispatchList)
+        {
+            // 7. Update visual feedback
+            $('#progressMessage').text(`Getting dispatch ${parseInt(i)+1} of ${dispatchList.length}`);
+
+            // 8. Execute Request
+            await fetch(`${url}?${uAgentParam}&q=dispatch;dispatchid=${dispatchList[i]}`)
+                .then(res => {
+                    if (res.status === 404) {
+                        throw Error(`Nation does not exist. (404)`)
+                    }
+                    return res.text()
+                })
+                .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+                .then(data => {
+                    textList.push(data.getElementsByTagName("TEXT")[0].innerHTML);
+                });
+
+            // 9. Update visual feedback
+            let percentProgress = Math.ceil((i/(dispatchList.length-1))*100)
+            progressBarPercent.attr('style',`width: ${percentProgress}%`);
+            progressBarPercent.text(`${percentProgress}%`)
+
+            await sleep(throttle); // Rate limit
+
+
+        }
+        console.log(textList);
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+
+    function sleep(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
+}
