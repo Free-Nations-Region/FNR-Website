8af5269aafc7eb4bd5e7c81e4debbb8d3a7933c9
Heaveria <me@heaveria.com>
2021-05-10 00:12:39 +0200

    JWT token async function. Includes getting the token, validating it, and reading its content.

--- en/login.js
+++ en/login.js
@@ -56,3 +56,45 @@ function loginSubmit()
             break;
     }
 }
+
+async function getNewJWT()
+{
+    // 1. Setup
+    let url = `https://api.the-fnr.com`;
+    let apiKey = `18d02a5f-32cd-4b9b-8aae-d32f8469f67a`;
+    let pubKey = `-----BEGIN PUBLIC KEY-----
+                    MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDiFdVE7Vh4kDdgZnY2sJxnMiwB
+                    dTlyWC1q2AsRmdxJq0oJI6obJFclacosx6iMonLK8qMcLSrDozWeALhpsVLkwRmC
+                    k9l696s9t8MRI0vUEBaCCCNWvllRKPCrEZ/UUDXBexfFllbP/bXM23AULCfbyB7d
+                    pGLCJiv3acFbmJXyFQIDAQAB
+                    -----END PUBLIC KEY-----`
+    let token;
+
+    /* 2. Validate Token.
+     *  Requires the jrsasign library. (https://github.com/kjur/jsrsasign)
+     */
+    await fetch(`${url}/v1/auth/token`,
+        {
+            method: 'GET',
+            headers: {
+                'apiKey': apiKey
+            }})
+        .then(res => res.json())
+        .then(data => {
+            token = data.token;
+        })
+        .catch(err => console.log(err));
+
+    // 3. Validate Token
+    let jwtKey = KEYUTIL.getKey(pubKey);
+    let isValid = KJUR.jws.JWS.verifyJWT(token, jwtKey, {alg: ['RS256']});
+
+    // 4. Read content of Token
+    let headerObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(token.split(".")[0]));
+    let payloadObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(token.split(".")[1]));
+    return {
+        'token': token,
+        'isValid': isValid,
+        'payload': payloadObj
+    }
+}
